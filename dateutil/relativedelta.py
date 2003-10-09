7484e70e19476af7c309644f91532a542dfa0189
niemeyer <>
2003-10-09 16:40:40 +0000

    Minor changes in __author__ variable.

--- dateutil/relativedelta.py
+++ dateutil/relativedelta.py
@@ -4,7 +4,7 @@ Copyright (c) 2003  Gustavo Niemeyer <niemeyer@conectiva.com>
 This module offers extensions to the standard python 2.3+
 datetime module.
 """
-__author__ = "Gustavo Niemeyer"
+__author__ = "Gustavo Niemeyer <niemeyer@conectiva.com>"
 __license__ = "PSF License"
 
 import datetime
--- dateutil/rrule.py
+++ dateutil/rrule.py
@@ -1,3 +1,12 @@
+"""
+Copyright (c) 2003  Gustavo Niemeyer <niemeyer@conectiva.com>
+
+This module offers extensions to the standard python 2.3+
+datetime module.
+"""
+__author__ = "Gustavo Niemeyer <niemeyer@conectiva.com>"
+__license__ = "PSF License"
+
 import datetime
 import calendar
 import thread
