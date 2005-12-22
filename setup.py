db8b18dcdf4b73ac83138a3b84d6b048b18ae06c
niemeyer <>
2005-12-22 15:02:22 +0000

    - Applying patch by Sidnei da Silva do install the zoneinfo file   correctly when using --prefix. - Updated project information.

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@ from distutils.sysconfig import get_python_lib
 from distutils.core import setup
 import glob
 import os
+from os.path import isfile, join
 
-if os.path.isfile("MANIFEST"):
+if isfile("MANIFEST"):
     os.unlink("MANIFEST")
 
-PYTHONLIB = get_python_lib(1)
+# Get PYTHONLIB with no prefix so --prefix installs work.
+PYTHONLIB = get_python_lib(1, 0, '')
+ZONEINFO = join("dateutil", "zoneinfo")
 
 setup(name="python-dateutil",
       version = "1.1",
       description = "Extensions to the standard python 2.3+ datetime module",
       author = "Gustavo Niemeyer",
-      author_email = "niemeyer@conectiva.com",
-      url = "https://moin.conectiva.com.br/DateUtil",
+      author_email = "gustavo@niemeyer.net",
+      url = "http://labix.org/python-dateutil",
       license = "PSF License",
       long_description =
 """\
@@ -22,6 +25,6 @@ The dateutil module provides powerful extensions to the standard
 datetime module, available in Python 2.3+.
 """,
       packages = ["dateutil", "dateutil.zoneinfo"],
-      data_files = [(PYTHONLIB+"/dateutil/zoneinfo", 
-                     glob.glob("dateutil/zoneinfo/zoneinfo*.tar.*"))],
+      data_files = [(join(PYTHONLIB, ZONEINFO),
+                     glob.glob(join(ZONEINFO, "zoneinfo*.tar.*")))],
       )
