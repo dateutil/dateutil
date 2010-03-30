30af0028a6633dc415a6c41bf2f2a67bda9bb68a
niemeyer <>
2010-03-30 00:54:11 +0000

    Preparing for 1.5.

--- dateutil/__init__.py
+++ dateutil/__init__.py
@@ -1,9 +1,9 @@
 """
-Copyright (c) 2003-2007  Gustavo Niemeyer <gustavo@niemeyer.net>
+Copyright (c) 2003-2010  Gustavo Niemeyer <gustavo@niemeyer.net>
 
 This module offers extensions to the standard python 2.3+
 datetime module.
 """
 __author__ = "Gustavo Niemeyer <gustavo@niemeyer.net>"
 __license__ = "PSF License"
-__version__ = "1.4.1"
+__version__ = "1.5"
--- dateutil/relativedelta.py
+++ dateutil/relativedelta.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2003-2007  Gustavo Niemeyer <gustavo@niemeyer.net>
+Copyright (c) 2003-2010  Gustavo Niemeyer <gustavo@niemeyer.net>
 
 This module offers extensions to the standard python 2.3+
 datetime module.
