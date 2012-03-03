40996e01996096f252ef9d7754edf054e691900d
Tomi Pieviläinen <tomi.pievilainen+launchpad@iki.fi>
2012-03-03 11:43:08 +0200

    Metadata fixes
    Fix remaining references to PSF license, change maintainer info.
    Removed most occurances of __author__, since it felt redundant
    with the copyright notice (credit) and package metadata
    (developer contact).
    
    Also fixed references to Python 2.3 (it's not even tested anymore)
    and changed calls from python3 to just python (let the system decide).

--- sandbox/scheduler.py
+++ sandbox/scheduler.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 """
 Copyright (c) 2003-2005  Gustavo Niemeyer <gustavo@niemeyer.net>
 
-This module offers extensions to the standard python 2.3+
+This module offers extensions to the standard Python
 datetime module.
 """
-__author__ = "Gustavo Niemeyer <gustavo@niemeyer.net>"
-__license__ = "PSF License"
+__author__ = "Tomi Pieviläinen <tomi.pievilainen@iki.fi>"
+__license__ = "Simplified BSD"
 
 import datetime
 import _thread
