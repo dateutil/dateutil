aefdb2b04c2a737afc049413b2ffa84a4db82d5e
Tomi Pieviläinen <tomi.pievilainen+launchpad@iki.fi>
2012-04-29 16:32:22 +0300

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -34,4 +34,16 @@ datetime module available in the Python standard library.
       zip_safe = False,
       requires = ["six"],
       install_requires = ["six"], # XXX fix when packaging is sane again
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Topic :: Software Development :: Libraries',
+      ]
       )
