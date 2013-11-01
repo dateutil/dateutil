8c6026ba09716a4e164f5420120bfe2ebb2d9d82
Tomi Pieviläinen <hukka@tsume>
2013-11-01 10:37:15 +0200

    Use /usr/bin/env, test for py33 and how to release in README.

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ datetime module available in the Python standard library.
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
           'Topic :: Software Development :: Libraries',
       ]
       )
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
 
