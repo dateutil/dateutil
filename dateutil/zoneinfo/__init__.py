61118923218429fffed2146997d8162fe3fee4d0
Yaron de Leeuw <me@jarondl.net>
2014-11-24 20:33:36 +0200

    many changes to zoneinfo, and to the download mechanizm

--- dateutil/zoneinfo/__init__.py
+++ dateutil/zoneinfo/__init__.py
@@ -1,57 +1,43 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
-from subprocess import call
+from subprocess import check_call
 from tarfile import TarFile
 
 from dateutil.tz import tzfile
 
 __all__ = ["setcachesize", "gettz", "rebuild"]
 
-CACHE = []
-CACHESIZE = 10
+_ZONEFILENAME = "dateutil-zoneinfo.tar.gz"
 
 class tzfile(tzfile):
     def __reduce__(self):
         return (gettz, (self._filename,))
 
 def getzoneinfofile():
-    filenames = sorted(os.listdir(os.path.join(os.path.dirname(__file__))))
-    filenames.reverse()
-    for entry in filenames:
-        if entry.startswith("zoneinfo") and ".tar." in entry:
-            return os.path.join(os.path.dirname(__file__), entry)
-    return None
+    zonefilename = os.path.join(os.path.dirname(__file__), _ZONEFILENAME)
+    if os.path.isfile(zonefilename):
+        return zonefilename
+    else:
+        return None
 
-ZONEINFOFILE = getzoneinfofile()
-
-del getzoneinfofile
-
-def setcachesize(size):
-    global CACHESIZE, CACHE
-    CACHESIZE = size
-    del CACHE[size:]
-
-def gettz(name):
-    tzinfo = None
-    if ZONEINFOFILE:
-        for cachedname, tzinfo in CACHE:
-            if cachedname == name:
-                break
+class ZoneInfoFile(object):
+    def __init__(self, zonefile=None):
+        self.zonefile = zonefile
+        if zonefile is not None and os.path.isfile(zonefile):
+            with TarFile.open(zonefile,'r') as tf:
+                self.zones = {zf.name: tzfile(tf.extractfile(zf))
+                              for zf in tf.getmembers() if zf.isfile()}
         else:
-            tf = TarFile.open(ZONEINFOFILE)
-            try:
-                zonefile = tf.extractfile(name)
-            except KeyError:
-                tzinfo = None
-            else:
-                tzinfo = tzfile(zonefile)
-            tf.close()
-            CACHE.insert(0, (name, tzinfo))
-            del CACHE[CACHESIZE:]
-    return tzinfo
+            self.zones = dict()
 
-def rebuild(filename, tag=None, format="gz"):
+_CLASS_ZONE_INSTANCE = ZoneInfoFile(getzoneinfofile())
+def gettz(name):
+    return _CLASS_ZONE_INSTANCE.zones.get(name)
+
+
+
+def rebuild(filename, tag=None, format="gz",zonegroups=[]):
     """Rebuild the internal timezone info in dateutil/zoneinfo/zoneinfo*tar*
 
     filename is the timezone tarball from ftp.iana.org/tz.
@@ -61,40 +47,26 @@ def rebuild(filename, tag=None, format="gz"):
     tmpdir = tempfile.mkdtemp()
     zonedir = os.path.join(tmpdir, "zoneinfo")
     moduledir = os.path.dirname(__file__)
-    if tag: tag = "-"+tag
-    targetname = "zoneinfo%s.tar.%s" % (tag, format)
     try:
-        tf = TarFile.open(filename)
-        # The "backwards" zone file contains links to other files, so must be
-        # processed as last
-        for name in sorted(tf.getnames(),
-                           key=lambda k: k != "backward" and k or "z"):
-            if not (name.endswith(".sh") or
-                    name.endswith(".tab") or
-                    name == "leapseconds"):
+        with TarFile.open(filename) as tf:
+            # The "backwards" zone file contains links to other files, so must be
+            # processed as last
+            for name in zonegroups:
                 tf.extract(name, tmpdir)
-                filepath = os.path.join(tmpdir, name)
-                try:
-                    # zic will return errors for nontz files in the package
-                    # such as the Makefile or README, so check_call cannot
-                    # be used (or at least extra checks would be needed)
-                    call(["zic", "-d", zonedir, filepath])
-                except OSError as e:
-                    if e.errno == 2:
-                        logging.error(
-                            "Could not find zic. Perhaps you need to install "
-                            "libc-bin or some other package that provides it, "
-                            "or it's not in your PATH?")
+            filepaths = [os.path.join(tmpdir, n) for n in zonegroups]
+            try:
+                check_call(["zic", "-d", zonedir] + filepaths)
+            except OSError as e:
+                if e.errno == 2:
+                    logging.error(
+                        "Could not find zic. Perhaps you need to install "
+                        "libc-bin or some other package that provides it, "
+                        "or it's not in your PATH?")
                     raise
-        tf.close()
-        target = os.path.join(moduledir, targetname)
-        for entry in os.listdir(moduledir):
-            if entry.startswith("zoneinfo") and ".tar." in entry:
-                os.unlink(os.path.join(moduledir, entry))
-        tf = TarFile.open(target, "w:%s" % format)
-        for entry in os.listdir(zonedir):
-            entrypath = os.path.join(zonedir, entry)
-            tf.add(entrypath, entry)
-        tf.close()
+        target = os.path.join(moduledir, _ZONEFILENAME)
+        with TarFile.open(target, "w:%s" % format) as tf:
+            for entry in os.listdir(zonedir):
+                entrypath = os.path.join(zonedir, entry)
+                tf.add(entrypath, entry)
     finally:
         shutil.rmtree(tmpdir)
--- updatezinfo.py
+++ updatezinfo.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 import os
 import hashlib
+import json
+import io
 
 from six.moves.urllib import request
 
 from dateutil.zoneinfo import rebuild
 
-
-# ad-hoc solution. TODO: use configuration file or something
-TZRELEASES = "ftp://ftp.iana.org/tz/releases/"
-TZFILEPATTERN = "tzdata{tag}.tar.gz"
-TZTAG = "2014j"
-SHA512 = "4c2979be3a96f91f8576304ec905d571b73df0842c8300c1d7317819b45ab3e29948ed911aa265b12a4ad587d5cba44f646dd02e40e4fbf9e68556a2d327142e"
-
-TZFILE = TZFILEPATTERN.format(tag=TZTAG)
+METADATA_FILE = "zonefile_metadata.json"
 
 def main():
-    if not os.path.isfile(TZFILE):
+    with io.open(METADATA_FILE,'r') as f:
+        metadata = json.load(f)
+
+    if not os.path.isfile(metadata['tzdata_file']):
         print("Downloading tz file from iana")
-        request.urlretrieve(TZRELEASES + TZFILE, TZFILE)
-    with open(TZFILE,'rb') as tzfile:
+        request.urlretrieve(os.path.join(metadata['releases_url'],metadata['tzdata_file']), metadata['tzdata_file'])
+    with open(metadata['tzdata_file'],'rb') as tzfile:
         sha_hasher = hashlib.sha512()
         sha_hasher.update(tzfile.read())
         sha_512_file = sha_hasher.hexdigest()
-        assert SHA512 == sha_512_file, "SHA failed for downloaded tz file"
+        assert metadata['tzdata_file_sha512'] == sha_512_file, "SHA failed for downloaded tz file"
     print("Updating timezone information...")
-    rebuild(TZFILE, TZTAG)
+    rebuild(metadata['tzdata_file'], zonegroups=metadata['zonegroups'])
     print("Done.")
 
 if __name__ == "__main__":
