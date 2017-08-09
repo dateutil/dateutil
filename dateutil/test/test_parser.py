04f35b9258d8d0c7c5ee7a92cc25b21f1196b9cf
Brock Mendel <jbrockmendel@gmail.com>
2017-08-09 10:54:24 -0700

    Merge test_parser_parametrized back into test_parser, add tests_requires pytest

--- dateutil/test/test_parser.py
+++ dateutil/test/test_parser.py
@@ -10,6 +10,35 @@ from dateutil.parser import parse, parserinfo
 from six import assertRaisesRegex, PY3
 from six.moves import StringIO
 
+import pytest
+
+
+# If we have a 4-digit year, a non-numeric month (abbreviated or not),
+# and a day (1 or 2 digits), then there is no ambiguity as to which
+# token is a year/month/day.  This holds regardless of what order the
+# terms are in and for each of the separators below.
+seps = ['-', ' ', '/', '.']
+token_opts = [['%Y'], ['%b', '%B'], ['%d', '%-d']]
+
+prods = itertools.product(*token_opts)
+perms = [y for x in prods for y in itertools.permutations(x)]
+unambig_fmts = [sep.join(perm) for sep in seps for perm in perms]
+
+@pytest.mark.parametrize("fmt", unambig_fmts)
+@pytest.mark.parametrize("year", [2003])
+@pytest.mark.parametrize("month", [9])
+@pytest.mark.parametrize("day", [25])
+def test_Ybd_permutations(fmt, year, month, day):
+    try:
+        actual = datetime(year, month, day)
+    except ValueError:
+        # e.g. Feb 30
+        return
+
+    dstr = actual.strftime(fmt)
+    res = parse(dstr, default=datetime(2003, 9, 25))
+    assert res == actual
+
 
 class ParserTest(unittest.TestCase):
 
--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ datetime module available in the Python standard library.
       package_data={"dateutil.zoneinfo": ["dateutil-zoneinfo.tar.gz"]},
       zip_safe=True,
       requires=["six"],
+      tests_require=["pytest"],
       install_requires=["six >=1.5"],  # XXX fix when packaging is sane again
       classifiers=[
           'Development Status :: 5 - Production/Stable',
