24fae16c8819ce82d43d57d6635c9f0b1d57d1da
Brock Mendel <jbrockmendel@gmail.com>
2017-11-01 17:50:45 -0700

    Refactor _parse_numeric_token out from the big parsing loop

--- dateutil/parser.py
+++ dateutil/parser.py
@@ -726,122 +726,8 @@ class parser(object):
                     value = None
 
                 if value is not None:
-                    # Token is a number
-                    len_li = len(l[i])
-
-                    if (len(ymd) == 3 and len_li in (2, 4) and
-                          res.hour is None and
-                          (i + 1 >= len_l or
-                          (l[i + 1] != ':' and
-                           info.hms(l[i + 1]) is None))):
-                        # 19990101T23[59]
-                        s = l[i]
-                        res.hour = int(s[:2])
-
-                        if len_li == 4:
-                            res.minute = int(s[2:])
-
-                    elif len_li == 6 or (len_li > 6 and l[i].find('.') == 6):
-                        # YYMMDD or HHMMSS[.ss]
-                        s = l[i]
-
-                        if not ymd and '.' not in l[i]:
-                            ymd.append(s[:2])
-                            ymd.append(s[2:4])
-                            ymd.append(s[4:])
-                        else:
-                            # 19990101T235959[.59]
-
-                            # TODO: Check if res attributes already set.
-                            res.hour = int(s[:2])
-                            res.minute = int(s[2:4])
-                            res.second, res.microsecond = self._parsems(s[4:])
-
-                    elif len_li in (8, 12, 14):
-                        # YYYYMMDD
-                        s = l[i]
-                        ymd.append(s[:4], 'Y')
-                        ymd.append(s[4:6])
-                        ymd.append(s[6:8])
-
-                        if len_li > 8:
-                            res.hour = int(s[8:10])
-                            res.minute = int(s[10:12])
-
-                            if len_li > 12:
-                                res.second = int(s[12:])
-
-                    elif self._find_hms_idx(i, l, info, allow_jump=True) is not None:
-                        # HH[ ]h or MM[ ]m or SS[.ss][ ]s
-                        hms_idx = self._find_hms_idx(i, l, info, allow_jump=True)
-                        (i, hms) = self._parse_hms(i, l, info, hms_idx)
-                        if hms is not None:
-                            # TODO: checking that hour/minute/second are not
-                            # already set?
-                            self._assign_hms(res, value_repr, hms)
-
-                    elif i + 2 < len_l and l[i + 1] == ':':
-                        # HH:MM[:SS[.ss]]
-                        res.hour = int(value)
-                        value = float(l[i + 2])  # TODO: try/except for this?
-                        (res.minute, res.second) = self._parse_min_sec(value)
-
-                        if i + 4 < len_l and l[i + 3] == ':':
-                            res.second, res.microsecond = self._parsems(l[i + 4])
-
-                            i += 2
-
-                        i += 2
-
-                    elif i + 1 < len_l and l[i + 1] in ('-', '/', '.'):
-                        sep = l[i + 1]
-                        ymd.append(value_repr)
-
-                        if i + 2 < len_l and not info.jump(l[i + 2]):
-                            if l[i + 2].isdigit():
-                                # 01-01[-01]
-                                ymd.append(l[i + 2])
-                            else:
-                                # 01-Jan[-01]
-                                value = info.month(l[i + 2])
-
-                                if value is not None:
-                                    ymd.append(value, 'M')
-                                else:
-                                    raise InvalidDatetimeError(timestr)
-
-                            if i + 3 < len_l and l[i + 3] == sep:
-                                # We have three members
-                                value = info.month(l[i + 4])
-
-                                if value is not None:
-                                    ymd.append(value, 'M')
-                                else:
-                                    ymd.append(l[i + 4])
-                                i += 2
-
-                            i += 1
-                        i += 1
-
-                    elif i + 1 >= len_l or info.jump(l[i + 1]):
-                        if i + 2 < len_l and info.ampm(l[i + 2]) is not None:
-                            # 12 am
-                            hour = int(value)
-                            res.hour = self._adjust_ampm(hour, info.ampm(l[i + 2]))
-                            i += 1
-                        else:
-                            # Year, month or day
-                            ymd.append(value)
-                        i += 1
-
-                    elif info.ampm(l[i + 1]) is not None and (0 <= value < 24):
-                        # 12am
-                        hour = int(value)
-                        res.hour = self._adjust_ampm(hour, info.ampm(l[i + 1]))
-                        i += 1
-
-                    elif not fuzzy:
-                        raise InvalidDatetimeError(timestr)
+                    # Numeric token
+                    i = self._parse_numeric_token(l, i, info, ymd, res, fuzzy)
 
                 # Check weekday
                 elif info.weekday(l[i]) is not None:
@@ -975,6 +861,128 @@ class parser(object):
             return res, tuple(skipped_tokens)
         else:
             return res, None
+
+    def _parse_numeric_token(self, tokens, idx, info, ymd, res, fuzzy):
+        # Token is a number
+        value_repr = tokens[idx]
+        value = float(value_repr)
+        len_li = len(value_repr)
+
+        if (len(ymd) == 3 and len_li in (2, 4) and
+            res.hour is None and
+            (idx + 1 >= len_l or
+             (tokens[idx + 1] != ':' and
+              info.hms(tokens[idx + 1]) is None))):
+            # 19990101T23[59]
+            s = tokens[idx]
+            res.hour = int(s[:2])
+
+            if len_li == 4:
+                res.minute = int(s[2:])
+
+        elif len_li == 6 or (len_li > 6 and tokens[idx].find('.') == 6):
+            # YYMMDD or HHMMSS[.ss]
+            s = tokens[idx]
+
+            if not ymd and '.' not in tokens[idx]:
+                ymd.append(s[:2])
+                ymd.append(s[2:4])
+                ymd.append(s[4:])
+            else:
+                # 19990101T235959[.59]
+
+                # TODO: Check if res attributes already set.
+                res.hour = int(s[:2])
+                res.minute = int(s[2:4])
+                res.second, res.microsecond = self._parsems(s[4:])
+
+        elif len_li in (8, 12, 14):
+            # YYYYMMDD
+            s = tokens[idx]
+            ymd.append(s[:4], 'Y')
+            ymd.append(s[4:6])
+            ymd.append(s[6:8])
+
+            if len_li > 8:
+                res.hour = int(s[8:10])
+                res.minute = int(s[10:12])
+
+                if len_li > 12:
+                    res.second = int(s[12:])
+
+        elif self._find_hms_idx(idx, tokens, info, allow_jump=True) is not None:
+            # HH[ ]h or MM[ ]m or SS[.ss][ ]s
+            hms_idx = self._find_hms_idx(idx, tokens, info, allow_jump=True)
+            (i, hms) = self._parse_hms(idx, tokens, info, hms_idx)
+            if hms is not None:
+                # TODO: checking that hour/minute/second are not
+                # already set?
+                self._assign_hms(res, value_repr, hms)
+
+        elif idx + 2 < len_l and tokens[idx + 1] == ':':
+            # HH:MM[:SS[.ss]]
+            res.hour = int(value)
+            value = float(tokens[idx + 2])  # TODO: try/except for this?
+            (res.minute, res.second) = self._parse_min_sec(value)
+
+            if idx + 4 < len_l and tokens[idx + 3] == ':':
+                res.second, res.microsecond = self._parsems(tokens[idx + 4])
+
+                idx += 2
+
+            idx += 2
+
+        elif idx + 1 < len_l and tokens[idx + 1] in ('-', '/', '.'):
+            sep = tokens[idx + 1]
+            ymd.append(value_repr)
+
+            if idx + 2 < len_l and not info.jump(tokens[idx + 2]):
+                if tokens[idx + 2].isdigit():
+                    # 01-01[-01]
+                    ymd.append(tokens[idx + 2])
+                else:
+                    # 01-Jan[-01]
+                    value = info.month(tokens[idx + 2])
+
+                    if value is not None:
+                        ymd.append(value, 'M')
+                    else:
+                        raise InvalidDatetimeError(timestr)
+
+                if idx + 3 < len_l and tokens[idx + 3] == sep:
+                    # We have three members
+                    value = info.month(tokens[idx + 4])
+
+                    if value is not None:
+                        ymd.append(value, 'M')
+                    else:
+                        ymd.append(tokens[idx + 4])
+                    idx += 2
+
+                idx += 1
+            idx += 1
+
+        elif idx + 1 >= len_l or info.jump(tokens[idx + 1]):
+            if idx + 2 < len_l and info.ampm(tokens[idx + 2]) is not None:
+                # 12 am
+                hour = int(value)
+                res.hour = self._adjust_ampm(hour, info.ampm(tokens[idx + 2]))
+                idx += 1
+            else:
+                # Year, month or day
+                ymd.append(value)
+            idx += 1
+
+        elif info.ampm(tokens[idx + 1]) is not None and (0 <= value < 24):
+            # 12am
+            hour = int(value)
+            res.hour = self._adjust_ampm(hour, info.ampm(tokens[idx + 1]))
+            idx += 1
+
+        elif not fuzzy:
+            raise InvalidDatetimeError(timestr)
+
+    return idx
 
     def _find_hms_idx(self, idx, tokens, info, allow_jump):
         len_l = len(tokens)
