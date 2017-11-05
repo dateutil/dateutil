39e0bf8ea2fe32b3077a7bb6370df968c7bbf42f
Paul Ganssle <paul@ganssle.io>
2017-11-05 15:40:50 -0500

    Add a tz.UTC object

--- dateutil/tz/__init__.py
+++ dateutil/tz/__init__.py
@@ -1,5 +1,8 @@
 from .tz import *
 
+UTC = tzutc()
+
 __all__ = ["tzutc", "tzoffset", "tzlocal", "tzfile", "tzrange",
            "tzstr", "tzical", "tzwin", "tzwinlocal", "gettz",
-           "enfold", "datetime_ambiguous", "datetime_exists"]
+           "enfold", "datetime_ambiguous", "datetime_exists",
+           "UTC"]
