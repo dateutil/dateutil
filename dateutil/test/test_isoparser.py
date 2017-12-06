42db26e1fe525fb1821fbb5cbb85aac35c62f32f
Paul Ganssle <paul@ganssle.io>
2017-12-06 23:52:19 +0000

    Make test_isoparser_byte_sep conditionally xfailin

--- dateutil/test/test_isoparser.py
+++ dateutil/test/test_isoparser.py
@@ -274,10 +274,11 @@ def test_isoparser_invalid_sep(sep):
         isoparser(sep=sep)
 
 
-@pytest.mark.xfail
+# This only fails on Python 3
+@pytest.mark.xfail(six.PY3, reason="Fails on Python 3 only")
 def test_isoparser_byte_sep():
     dt = datetime(2017, 12, 6, 12, 30, 45)
-    dt_str = dt.isoformat(sep='T')
+    dt_str = dt.isoformat(sep=str('T'))
 
     dt_rt = isoparser(sep=b'T').isoparse(dt_str)
 
