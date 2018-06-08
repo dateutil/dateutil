47fac7dc27805e65bdf0447ac0de25394b0a7bad
Alex Willmer <alex@moreati.org.uk>
2018-06-08 17:47:21 +0100

    Fix Python 3.3 on Travis (#1)
    * Conditionally enable --doctest-continue-on-failure (when available)
    
    This fixes the Travis build on Python 3.3
    
    * Don't silently use a wrong Python version with Tox 3.0.0+
    
    * Fix creation of 'docs' Tox environment

--- conftest.py
+++ conftest.py
@@ -6,3 +6,11 @@ if sys.platform != 'win32':
         'dateutil/tzwin.py',
         'dateutil/tz/win.py',
     ]
+
+# We would like to use --doctest-continue-on-failure. However this option was
+# added in pytest 3.5.0, and pytest >= 3.3.0 does not support Python 3.3.
+# Therefore we enable the feature here.
+
+def pytest_configure(config):
+    if config.getvalue('doctestmodules'):
+        config.option.doctest_continue_on_failure = True
