a4799a98b1085a49dcb43f8616a070ad0f10f53e
Paul Ganssle <paul@ganssle.io>
2021-09-17 10:35:51 -0400

    Add support for key property

--- tests/property/test_tz_prop.py
+++ tests/property/test_tz_prop.py
@@ -12,8 +12,16 @@ NEGATIVE_EPOCHALYPSE = datetime.fromtimestamp(0) - timedelta(seconds=2147483648)
 
 
 @pytest.mark.gettz
+@given(key=st.timezone_keys(allow_prefix=True))
+def test_key_property(key):
+    tzi = tz.gettz(key)
+    assume(isinstance(tzi, tz.tzfile))
+    assert tzi.key == key
+
+
+@pytest.mark.gettz
 @pytest.mark.skipif(
-    sys.version_info < (3, 6), reason="Not supported on Python 2"
+    sys.version_info < (3, 6), reason="Not supported on Python < 3.6"
 )
 @pytest.mark.parametrize("gettz_arg", [None, ""])
 # TODO: Remove bounds when GH #590 is resolved
