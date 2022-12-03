7a9dc781039a06930186512b21b5110e892b6513
Paul Ganssle <git@m.ganssle.io>
2022-12-03 12:40:58 -0500

    Add available_iana_zones function

--- src/dateutil/tz/__init__.py
+++ src/dateutil/tz/__init__.py
@@ -2,10 +2,25 @@
 from .tz import *
 from .tz import __doc__
 
-__all__ = ["tzutc", "tzoffset", "tzlocal", "tzfile", "tzrange",
-           "tzstr", "tzical", "tzwin", "tzwinlocal", "gettz",
-           "enfold", "datetime_ambiguous", "datetime_exists",
-           "resolve_imaginary", "UTC", "DeprecatedTzFormatWarning"]
+__all__ = [
+    "tzutc",
+    "tzoffset",
+    "tzlocal",
+    "tzfile",
+    "tzrange",
+    "tzstr",
+    "tzical",
+    "tzwin",
+    "tzwinlocal",
+    "gettz",
+    "enfold",
+    "datetime_ambiguous",
+    "datetime_exists",
+    "resolve_imaginary",
+    "available_iana_timezones",
+    "UTC",
+    "DeprecatedTzFormatWarning",
+]
 
 
 class DeprecatedTzFormatWarning(Warning):
